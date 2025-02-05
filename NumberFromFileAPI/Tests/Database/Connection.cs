f99e5ffa5f460aec5c8ecc59491a6d3cf74dd4b7
Blaga Mihai <mihai.blaga@codecrafters.ro>
2025-02-05 15:10:16 +0200

    Added the testing project.

---
+++ NumberFromFileAPI/Tests/Database/Connection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.Sqlite;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Tests.Database
+{
+    public class Connection
+    {
+        private string _connectionString = "Data Source=NumbersFromString;";
+
+        public Connection()
+        {
+            SQLitePCL.Batteries.Init();
+        }
+
+        [Fact]
+        public void CanConnectToSQLiteDatabase_ReturnsTrue_WhenConnectionSuccessful()
+        {
+            var connection = new SqliteConnection(_connectionString);
+
+            try
+            {
+                connection.Open(); 
+                Assert.Equal(System.Data.ConnectionState.Open, connection.State); 
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Could not establish connection: {ex.Message}");
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
